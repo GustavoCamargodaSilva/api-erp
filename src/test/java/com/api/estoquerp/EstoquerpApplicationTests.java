4f6f88a1ac5a76caceedf01796abdca50e0d6e11
GustavoCamargodaSilva <gustavocamargodasilva@hotmail.com>
2024-08-02 01:31:37 -0300

    Initial commit

---
+++ src/test/java/com/api/estoquerp/EstoquerpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.estoquerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoquerpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
