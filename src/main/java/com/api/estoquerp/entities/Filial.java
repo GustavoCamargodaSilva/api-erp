348d2de1a745c93ce7170ff307e59388445dbf28
GustavoCamargodaSilva <gustavocamargodasilva@hotmail.com>
2024-08-03 23:44:09 -0300

    criacaoFilial

---
+++ src/main/java/com/api/estoquerp/entities/Filial.java
@@ -0,0 +1,51 @@
+package com.api.estoquerp.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "filial")
+@Getter
+@Setter
+public class Filial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String apelido;
+    private String telefone;
+    private String email;
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String unidade;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
+
+
+    public Filial() { }
+
+    public Filial(Long id, String apelido, String telefone, String email, String cep, String logradouro, String complemento, String unidade, String bairro, String localidade, String uf, String ibge, String gia, String ddd, String siafi) {
+        this.id = id;
+        this.apelido = apelido;
+        this.telefone = telefone;
+        this.email = email;
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.complemento = complemento;
+        this.unidade = unidade;
+        this.bairro = bairro;
+        this.localidade = localidade;
+        this.uf = uf;
+        this.ibge = ibge;
+        this.gia = gia;
+        this.ddd = ddd;
+        this.siafi = siafi;
+    }
+}
